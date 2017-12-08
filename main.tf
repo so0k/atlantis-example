2b2c7f68c6c42f195836e4873f0eaa3e71dfd1d6
so0k <vincent.drl@gmail.com>
2017-12-08 14:01:52 +0800

    Add aws instance

--- main.tf
+++ main.tf
@@ -1,2 +1,15 @@
-resource "null_resource" "example" {
+provider "aws" {
+  version = "~> 1.5"
+  region  = "ap-southeast-1"
+}
+
+provider "null" {
+  version = "~> 1.0"
+}
+
+resource "null_resource" "example" {}
+
+resource "aws_instance" "web" {
+  ami           = "ami-032fb460"
+  instance_type = "t2.micro"
 }
